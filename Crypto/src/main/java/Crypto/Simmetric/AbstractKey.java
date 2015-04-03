36daf3c4ad3a2a87edd37684b4fe688589c4ee83
NFetissow <nfetissow@gmail.com>
2015-04-04 01:52:35 +0300

    toByte(), fromByte()

--- Crypto/src/main/java/Crypto/Simmetric/AbstractKey.java
+++ Crypto/src/main/java/Crypto/Simmetric/AbstractKey.java
@@ -1,10 +1,63 @@
 package Crypto.Simmetric;
 
-import java.io.Serializable;
-
-/**
- * Created by Никита on 28.03.2015.
- */
+import java.io.*;
 public abstract class AbstractKey implements Serializable {
     abstract public String getInfo();
+
+    public byte[] toBytes() {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = null;
+        try {
+            out = new ObjectOutputStream(bos);
+            out.writeObject(this);
+            return bos.toByteArray();
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+            return null;
+        }
+        finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+            try {
+                bos.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+    }
+
+    public static AbstractKey fromBytes(byte[] bytes) {
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInput in = null;
+        try {
+            in = new ObjectInputStream(bis);
+            AbstractKey key = (AbstractKey) in.readObject();
+            return key;
+        } catch (IOException ioEx) {
+            ioEx.printStackTrace();
+            return null;
+        } catch (ClassNotFoundException classNotFoundEx) {
+            classNotFoundEx.printStackTrace();
+            return null;
+        }
+        finally {
+            try {
+                bis.close();
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ex) {
+                // ignore close exception
+            }
+        }
+    }
 }
